9882f425145ab5d0d3eef0745367a264bfb1e564
ranjanistic <priyanshuranjan88@gmail.com>
2019-11-13 22:55:40 +0530

    Updated with more feats.
    + stop button
    + snackbar feedback
    + timer display

--- app/src/main/java/com/srec/vocorder/MainActivity.java
+++ app/src/main/java/com/srec/vocorder/MainActivity.java
@@ -2,65 +2,79 @@ package com.srec.vocorder;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.solver.widgets.Snapshot;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.ContactsContract;
+import android.os.SystemClock;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.io.File;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
-import javax.xml.transform.OutputKeys;
 
 public class MainActivity extends AppCompatActivity {
-
     public static final String TAG = "MainActivity";
     private MediaRecorder mdr;
     private static final int REQUEST_CODE_PERMISSIONS = 0;
     private String outFile;
+    Animation animView, animHide;
+    Boolean isOpen = false, stopFlag = true;
     private String[] permissions =  {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final TextView subtext = findViewById(R.id.subText);
+        final Chronometer timeView = findViewById(R.id.timer);
         final ImageButton startrec = findViewById(R.id.startrecbutt);
-        final Button stoprec = findViewById(R.id.stopRecButt);
-        stoprec.setEnabled(false);
-        stoprec.setBackgroundColor(getColor(R.color.colorAccent));
+        final ImageButton stoprec = findViewById(R.id.stopRecButt);
         startrec.setEnabled(true);
         outFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/vocorderAudioFile.3gp";
-        ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_CODE_PERMISSIONS);
-
+        animView = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        animHide = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
         startrec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(permissionsAccepted(permissions)){
+                    subtext.setText(getResources().getText(R.string.whileRecord));
+                    timeView.startAnimation(animView);
+                    stopFlag = false;
+                    if(isOpen){
+                        stoprec.startAnimation(animHide);
+                        stoprec.setClickable(false);
+                        isOpen = false;
+                    } else {
+                        stoprec.startAnimation(animView);
+                        stoprec.setClickable(true);
+                        isOpen = true;
+                    }
+
+                    Snackbar.make(view, Html.fromHtml("<font color=\"#aaaa00\">Recording started.</font>"), 5000)
+                            .setAction("OKAY", null)
+                            .setActionTextColor(Color.WHITE)
+                            .show();
                     startVoRec();
                     startrec.setEnabled(false);
-                    stoprec.setEnabled(true);
-                    stoprec.setBackgroundColor(getColor(R.color.activeColor));
                 } else {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.dialogTheme);
                     builder.setTitle("Permissions Required")
-                            .setIcon(R.drawable.ic_launcher_foreground)
-                            .setMessage("Audio permission is required to access your device mic.\nStorage permission is required to save recordings on your device")
-                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            .setIcon(R.drawable.ic_openmic)
+                            .setMessage("\nAudio permission is required to access your device mic.\n\nStorage permission is required to save recordings on your device")
+                            .setNegativeButton(R.string.denytext, null)
+                            .setPositiveButton(R.string.accepText, new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
                                     ActivityCompat.requestPermissions(MainActivity.this,permissions, REQUEST_CODE_PERMISSIONS);
                                 }
@@ -74,9 +88,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 stopVoRec();
+                subtext.setText(getResources().getText(R.string.startRecord));
                 startrec.setEnabled(true);
-                stoprec.setEnabled(false);
-                stoprec.setBackgroundColor(getColor(R.color.colorAccent));
+                stoprec.startAnimation(animHide);
+                timeView.startAnimation(animHide);
+                stoprec.setClickable(false);
+                Snackbar.make(view, Html.fromHtml("<font color=\"#00aaaa\">Recording stopped, saved as 'vocorderAudioFile.3gp'.</font>"), 5000)
+                        .setAction("OKAY", null)
+                        .setActionTextColor(Color.YELLOW)
+                        .show();
+                isOpen = false;
+                stopFlag = true;
             }
         });
     }
@@ -88,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         mdr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         mdr.setOutputFile(outFile);
         try{
-            mdr.prepare();;
+            mdr.prepare();
         } catch (IOException e){
             Log.e(TAG, "startVoRec: " + e.toString());
         }
@@ -111,5 +133,12 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+    public void startChrono(View view) {
+        ((Chronometer) findViewById(R.id.timer)).start();
+    }
+
+    public void stopChrono(View view) {
+        ((Chronometer) findViewById(R.id.timer)).stop();
     }
 }
--- app/src/main/java/com/srec/vocorder/Splash.java
+++ app/src/main/java/com/srec/vocorder/Splash.java
@@ -2,6 +2,9 @@ package com.srec.vocorder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
@@ -9,6 +12,12 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        //Window window = this.getWindow();
+        //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //window.setNavigationBarColor(this.getResources().getColor(R.color.colorPrimary));
+
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         finish();
